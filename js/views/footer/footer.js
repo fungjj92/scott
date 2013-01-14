b78a5cffb7e814613143c1d06f1606297f7346cd
Thomas Levine <occurrence@thomaslevine.com>
2013-01-14 17:28:47 -0600

    remove springy

--- js/views/footer/footer.js
+++ js/views/footer/footer.js
@@ -4,8 +4,7 @@ define([
   'backbone',
   'events',
   'text!templates/footer/footer.html',
-  'libs/springy/springyui'
-], function($, _, Backbone, Events, footerTemplate, Springy){
+], function($, _, Backbone, Events, footerTemplate){
   var FooterView = Backbone.View.extend({
     el: '.footer',
     intialize: function () {
@@ -17,28 +16,7 @@ define([
       Events.on('viewCreated', this.renderSpringy, this);
     },
     renderSpringy: function () {
-      var that = this;
-      var graph = new Springy();
-      var generateGraph = function (context, parentName, first) {
-        if(typeof first === 'undefined'){
-          var first = graph.newNode({label: parentName});
-        }
-        _.each(context.children, function(view, viewname) {
-          var second = graph.newNode({label: viewname + ' (' + view.cid + ')'});
-          graph.newEdge(first, second, {color: '#000'});
-          generateGraph(view, viewname, second);
-        });
-        return;
-      };
-      
-      generateGraph(this.options.appView, 'AppView');
-      
-      $('#springydemo').remove();
-      $('.springy-container').html('<canvas id="springydemo" width="960px" height="820"></canvas">');
-      var springy = $('#springydemo');
-      springy.springy({
-        graph: graph
-      });
+      console.log('This is where the springy graph had been rendered.')
     },
     clean: function () {
       Events.off('viewCreated', this.renderSpringy);
