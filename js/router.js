b9446ae6fa3e2944070c208274c78725f6be41e0
Thomas Levine <occurrence@thomaslevine.com>
2013-01-14 17:06:55 -0600

    routes

--- js/router.js
+++ js/router.js
@@ -7,14 +7,13 @@ define([
 ], function ($, _, Backbone, Vm) {
   var AppRouter = Backbone.Router.extend({
     routes: {
-      // Pages
-      'modules': 'modules',
-      'optimize': 'optimize',
-      'backbone/:section': 'backbone',
-      'backbone': 'backbone',
-      'manager': 'manager',
-      
-      // Default - catch all
+      // List of applications
+      'applications': 'applications',
+
+      // Everything about one application
+      'applications/:permitApplicationNumber': 'application'
+
+      // Homepage/dashboard for everything else
       '*actions': 'defaultAction'
     }
   });
@@ -22,34 +21,22 @@ define([
   var initialize = function(options){
     var appView = options.appView;
     var router = new AppRouter(options);
-    router.on('route:optimize', function () {
-      require(['views/optimize/page'], function (OptimizePage) {
-        var optimizePage = Vm.create(appView, 'OptimizePage', OptimizePage);
-        optimizePage.render();
-      });
-    });
     router.on('route:defaultAction', function (actions) {
       require(['views/dashboard/page'], function (DashboardPage) {
         var dashboardPage = Vm.create(appView, 'DashboardPage', DashboardPage);
         dashboardPage.render();
       });
     });
-    router.on('route:modules', function () {
-     require(['views/modules/page'], function (ModulePage) {
-        var modulePage = Vm.create(appView, 'ModulesPage', ModulePage);
-        modulePage.render();
+    router.on('route:applications', function () {
+      require(['views/applications/page'], function (ApplicationsPage) {
+        var applicationsPage = Vm.create(appView, 'ApplicationsPage', ApplicationsPage);
+        applicationsPage.render();
       });
     });
-    router.on('route:backbone', function (section) {
-      require(['views/backbone/page'], function (BackbonePage) {
-        var backbonePage = Vm.create(appView, 'BackbonePage', BackbonePage, {section: section});
-        backbonePage.render();
-      });
-    });
-    router.on('route:manager', function () {
-      require(['views/manager/page'], function (ManagerPage) {
-        var managerPage = Vm.create(appView, 'ManagerPage', ManagerPage);
-        managerPage.render();
+    router.on('route:application', function (permitApplicationNumber) {
+      require(['views/application/page'], function (ApplicationPage) {
+        var applicationPage = Vm.create(appView, 'ApplicationPage', ApplicationPage, {permitApplicationNumber: permitApplicationNumber});
+        applicationPage.render();
       });
     });
     Backbone.history.start();
