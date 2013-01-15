482b1fa9ea3cf3aae52c65daf949549e1114939b
Thomas Levine <occurrence@thomaslevine.com>
2013-01-15 16:05:42 -0600

    test record

---
+++ server/test.sql
@@ -0,0 +1,28 @@
+INSERT INTO application VALUES (
+  -- Bookkeeping
+  'MVN-2012-1266-CU',
+  1,
+
+  -- Automatic
+  'Barge fleeting operation in Mississippi River, Mile176 in ASCENSION PARISH',
+  'Carline Management',
+  'Doris Terrell',
+  '504-862-1588',
+  'Doris.Terrell@usace.army.mil',
+  '2012-08-06',
+  '2012-09-05',
+  '',
+  '',
+  'Ascension',
+  '',
+  '',
+
+  -- Manual
+  NULL,
+  NULL,
+  '',
+  '',
+  0,
+  NULL
+);
+
