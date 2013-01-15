c4feb391d81ffb4e0eaf7bd5f6413848128922ef
Thomas Levine <occurrence@thomaslevine.com>
2013-01-15 13:03:27 -0600

    migration

---
+++ server/migrate.sql
@@ -0,0 +1,6 @@
+ALTER TABLE application ADD COLUMN longitude FLOAT;
+ALTER TABLE application ADD COLUMN latitude FLOAT;
+ALTER TABLE application ADD COLUMN parish TEXT;
+ALTER TABLE application ADD COLUMN notes TEXT;
+ALTER TABLE application ADD COLUMN status TEXT;
+ALTER TABLE application ADD COLUMN flagged INTEGER;
