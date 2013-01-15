fa6331598713dcc25a0fa4e827b11c2dccee9495
Thomas Levine <occurrence@thomaslevine.com>
2013-01-15 08:26:53 -0600

    oeu

--- server/app.js
+++ server/app.js
@@ -4,6 +4,9 @@ var server = restify.createServer()
 
 server.listen(8080)
 
+// Convert this into a list of json dicts.
+// SELECT * FROM application;
+
 server.put('/applications/:permitApplicationNumber', function (req, res, next) {
     res.send(200)
     return next()
@@ -14,7 +17,8 @@ server.get('/applications/:permitApplicationNumber', function (req, res, next) {
 })
 
 // Convert this into a json dict.
-// SELECT * FROM application;
+// SELECT * FROM application WHERE permitApplicationNumber = ?;
+
 server.put('/applications', function (req, res, next) {
     res.send(200)
     return next()
