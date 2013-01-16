1842b7349dd01ac092fd8600379464d0a480cdec
Thomas Levine <occurrence@thomaslevine.com>
2013-01-15 20:43:37 -0600

    fetch the collection

--- client/js/views/applications/page.js
+++ client/js/views/applications/page.js
@@ -2,13 +2,19 @@ define([
   'jquery',
   'lodash',
   'backbone',
-  'text!templates/applications/page.html'
-], function($, _, Backbone, applicationsPageTemplate){
+  'text!templates/applications/page.html',
+  'collections/applications'
+], function($, _, Backbone, applicationsPageTemplate, ApplicationsCollection){
   var ApplicationsPage = Backbone.View.extend({
     el: '.page',
     render: function () {
       this.$el.html(applicationsPageTemplate);
+
+      var applicationsCollection = new ApplicationsCollection;
+      applicationsCollection.fetch()
+      console.log(applicationsCollection)
     }
   });
+
   return ApplicationsPage;
 });
