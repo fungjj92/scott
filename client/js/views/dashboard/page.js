2122ea11b95c29e7f7bd45449b51b5e36970592f
Thomas Levine <occurrence@thomaslevine.com>
2013-01-21 23:04:05 -0600

    works

--- client/js/views/dashboard/page.js
+++ client/js/views/dashboard/page.js
@@ -8,43 +8,47 @@ define([
 ], function($, _, Backbone, L, ApplicationsCollection, dashboardPageTemplate){
   var DashboardPage = Backbone.View.extend({
     el: '.page',
-    collection: new ApplicationsCollection(),
     render: function () {
       $(this.el).html(dashboardPageTemplate)
-      this.collection.fetch()
-      this.drawMap()
-    },
-    drawMap: function() {
-      var osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
-      var osmAttrib='Map data (C) OpenStreetMap contributors'
-      var osm = new L.TileLayer(osmUrl, {minZoom: 5, maxZoom: 12, attribution: osmAttrib})
-      var map = L.map('map').setView([28, -89.5], 6).addLayer(osm)
-      window.c = this.collection
-      var i
-      for (i in this.collection.models) {
-        application = this.collection.models[i]
-        console.log(application)
-        if (application.get('latitude')) {
-          var coords = [model.get('latitude'), model.get('longitude')]
-          var radius = application.get('acreage')
-     //   var color = {
-     //     impact: 'red',
-     //     mitigation: 'brown',
-     //     restoration: 'green'
-     //   }[application.get('type')]
-     //   if (!color) {
-     //       color = 'grey'
-     //   }
-          var circle = L.circle(coords, radius, {
-     //       'color': color,
-              color: 'black',
-              opacity: 0,
-              fillColor: 'black',
-              fillOpacity: 0.5
-          }).addTo(map)
+      applicationsCollection = new ApplicationsCollection
+      var page = this
+      applicationsCollection.fetch({
+        success: function(collection, response, options) {
+          var osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+          var osmAttrib='Map data (C) OpenStreetMap contributors'
+          var osm = new L.TileLayer(osmUrl, {minZoom: 5, maxZoom: 12, attribution: osmAttrib})
+          var map = L.map('map').setView([28, -89.5], 6).addLayer(osm)
+          var i
+          for (i in collection.models) {
+            application = collection.models[i]
+            if (application.get('latitude')) {
+              var coords = [
+                application.get('latitude'),
+                application.get('longitude')
+              ]
+              // Convert to square meters and then to radius
+              var tau = 6.283185
+              var radius = Math.pow( (application.get('acreage')  * 4046.86) / (0.5 * tau), 0.5 )
+              var adjustment = 100
+              var color = {
+                impact: 'red',
+                mitigation: 'brown',
+                restoration: 'green'
+              }[application.get('type')]
+              if (!color) {
+                  color = 'grey'
+              }
+              var circle = L.circle(coords, radius * adjustment, {
+                  color: 'black',
+                  opacity: 0,
+                  fillColor: color,
+                  fillOpacity: 0.5
+              }).addTo(map)
+            }
+          }
         }
       })
     }
-  });
-  return DashboardPage;
+  })
+  return DashboardPage
 });
