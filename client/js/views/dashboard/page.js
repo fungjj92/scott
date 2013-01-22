3e4156ab79582b76501a65f17e3128406854d308
Thomas Levine <occurrence@thomaslevine.com>
2013-01-21 20:10:10 -0600

    blarg

--- client/js/views/dashboard/page.js
+++ client/js/views/dashboard/page.js
@@ -2,13 +2,48 @@ define([
   'jquery',
   'lodash',
   'backbone',
+  'leaflet',
+  'collections/applications',
   'text!templates/dashboard/page.html'
-], function($, _, Backbone, dashboardPageTemplate){
+], function($, _, Backbone, L, ApplicationsCollection, dashboardPageTemplate){
   var DashboardPage = Backbone.View.extend({
     el: '.page',
+    collection: new ApplicationsCollection(),
     render: function () {
-
-      $(this.el).html(dashboardPageTemplate);
+      $(this.el).html(dashboardPageTemplate)
+      this.collection.fetch()
+      this.drawMap()
+    },
+    drawMap: function() {
+      var osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+      var osmAttrib='Map data (C) OpenStreetMap contributors'
+      var osm = new L.TileLayer(osmUrl, {minZoom: 5, maxZoom: 12, attribution: osmAttrib})
+      var map = L.map('map').setView([28, -89.5], 6).addLayer(osm)
+      window.c = this.collection
+      var i
+      for (i in this.collection.models) {
+        application = this.collection.models[i]
+        console.log(application)
+        if (application.get('latitude')) {
+          var coords = [model.get('latitude'), model.get('longitude')]
+          var radius = application.get('acreage')
+     //   var color = {
+     //     impact: 'red',
+     //     mitigation: 'brown',
+     //     restoration: 'green'
+     //   }[application.get('type')]
+     //   if (!color) {
+     //       color = 'grey'
+     //   }
+          var circle = L.circle(coords, radius, {
+     //       'color': color,
+              color: 'black',
+              opacity: 0,
+              fillColor: 'black',
+              fillOpacity: 0.5
+          }).addTo(map)
+        }
+      })
     }
   });
   return DashboardPage;
