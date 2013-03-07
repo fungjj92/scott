2f6b937bbfb1c826470ae084310267211843cc69
Thomas Levine <occurrence@thomaslevine.com>
2013-03-07 11:10:22 -0500

    url

--- migrations/2013-03-07.sql
+++ migrations/2013-03-07.sql
@@ -1,4 +1,4 @@
 -- I manually copied the results of this query.
 .mode column
 .width 70 10 10 10 10 10 10 70 10
-SELECT permitApplicationNumber,CUP,WQC,longitude,latitude,acreage,type,notes,status FROM application WHERE status != 1; 
+SELECT 'http://scott.thomaslevine.com/#/applications/' || permitApplicationNumber,CUP,WQC,longitude,latitude,acreage,type,notes,status FROM application WHERE status != 1; 
