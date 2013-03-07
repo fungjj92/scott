9a18a3d574321b93dbaed4b88e43d1ee50086896
Thomas Levine <occurrence@thomaslevine.com>
2013-03-07 11:09:29 -0500

    data

--- migrations/2013-03-07.sql
+++ migrations/2013-03-07.sql
@@ -1,2 +1,4 @@
 -- I manually copied the results of this query.
+.mode column
+.width 70 10 10 10 10 10 10 70 10
 SELECT permitApplicationNumber,CUP,WQC,longitude,latitude,acreage,type,notes,status FROM application WHERE status != 1; 
