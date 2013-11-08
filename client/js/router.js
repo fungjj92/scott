78a6010c5930f52bb689723a04dadb38917a9f5c
Thomas Levine <occurrence@thomaslevine.com>
2013-11-08 11:04:40 -0800

    router.js

--- client/js/router.js
+++ client/js/router.js
@@ -1,51 +1,48 @@
-// Filename: router.js
-define([
-  'jquery',
-  'underscore',
-  'backbone',
-  'vm'
-], function ($, _, Backbone, Vm) {
-  var AppRouter = Backbone.Router.extend({
-    routes: {
-      // List of applications
-      'applications': 'applications',
+var $ = require('jquery'),
+  , _ = require('underscore')
+  , Backbone = require('backbone')
+  , Vm = require('vm')
 
-      // Everything about one application
-      'applications/:permitApplicationNumber': 'application',
+var AppRouter = Backbone.Router.extend({
+  routes: {
+    // List of applications
+    'applications': 'applications',
 
-      // Homepage/dashboard for everything else
-      '*actions': 'defaultAction'
-    }
-  })
+    // Everything about one application
+    'applications/:permitApplicationNumber': 'application',
 
-  var initialize = function(options){
-    var appView = options.appView
-    var router = new AppRouter(options)
-    router.on('route:defaultAction', function (actions) {
-      require(['views/dashboard/page'], function (DashboardPage) {
-        var dashboardPage = Vm.create(appView, 'DashboardPage', DashboardPage)
-        dashboardPage.render()
-      })
-    })
-    router.on('route:applications', function () {
-      require(['views/applications/page'], function (ApplicationsPage) {
-        var applicationsPage = Vm.create(appView, 'ApplicationsPage', ApplicationsPage)
-        applicationsPage.render()
-      })
-    })
-    router.on('route:application', function (permitApplicationNumber) {
-      require(['views/application/page'], function (ApplicationPage) {
-        var applicationPage = Vm.create(appView, 'ApplicationPage', ApplicationPage, {permitApplicationNumber: permitApplicationNumber})
-        applicationPage.$model.fetch({
-          success: function(){
-            applicationPage.render()
-          }
-        })
-      })
-    })
-    Backbone.history.start()
-  }
-  return {
-    initialize: initialize
+    // Homepage/dashboard for everything else
+    '*actions': 'defaultAction'
   }
 })
+
+var initialize = function(options){
+  var appView = options.appView
+  var router = new AppRouter(options)
+  router.on('route:defaultAction', function (actions) {
+    require(['views/dashboard/page'], function (DashboardPage) {
+      var dashboardPage = Vm.create(appView, 'DashboardPage', DashboardPage)
+      dashboardPage.render()
+    })
+  })
+  router.on('route:applications', function () {
+    require(['views/applications/page'], function (ApplicationsPage) {
+      var applicationsPage = Vm.create(appView, 'ApplicationsPage', ApplicationsPage)
+      applicationsPage.render()
+    })
+  })
+  router.on('route:application', function (permitApplicationNumber) {
+    require(['views/application/page'], function (ApplicationPage) {
+      var applicationPage = Vm.create(appView, 'ApplicationPage', ApplicationPage, {permitApplicationNumber: permitApplicationNumber})
+      applicationPage.$model.fetch({
+        success: function(){
+          applicationPage.render()
+        }
+      })
+    })
+  })
+  Backbone.history.start()
+}
+return {
+  initialize: initialize
+}
