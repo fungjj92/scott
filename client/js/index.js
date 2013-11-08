81534cab0ea3694f65a1fcd9e5cdd92f99aac4dd
Thomas Levine <occurrence@thomaslevine.com>
2013-11-08 11:01:38 -0800

    main to index

---
+++ client/js/index.js
@@ -0,0 +1,15 @@
+var jquery       = require('jquery')
+  , underscore   = require('underscore')
+  , lodash       = require('lodash')
+  , backbone     = require('backbone')
+  , localstorage = require('localstorage')
+  , leaflet      = require('leaflet')
+  , d3           = require('d3')
+
+var AppView = require('./views/app.js')
+  , router  = require('./router.js')
+  , vm      = require('./vm.js')
+
+var appView = Vm.create({}, 'AppView', AppView)
+appView.render()
+Router.initialize({appView: appView})  // The router now has a copy of all main appview
