2387104d5e679a12ef35d051851c9294c8f8a03e
Thomas Levine <occurrence@thomaslevine.com>
2013-11-11 10:26:34 -0800

    set -e

--- client/js/collections/applications.js
+++ client/js/collections/applications.js
@@ -1,7 +1,7 @@
 var $ = require('jquery-browserify')
   , _ = require('lodash')
   , Backbone = require('backbone')
-  , applicationsModel = require('../../models/application.js')
+  , applicationsModel = require('../models/application.js')
 
 var applicationsCollection = Backbone.Collection.extend({
   model: applicationsModel,
